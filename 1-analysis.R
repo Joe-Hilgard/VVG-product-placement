f564aeec405819dfce5881459913e5686792204e
Joe Hilgard <jhilgard@asc.upenn.edu>
2015-12-21 14:55:42 -0500

    Bayes factor model comparisons.

--- 1-analysis.R
+++ 1-analysis.R
@@ -5,7 +5,7 @@ library(psych)
 library(BayesFactor)
 library(ggplot2)
 
-dat = read_excel("data_final.xls", 1)
+dat = read.delim("processed_data.txt", stringsAsFactors = F)
 
 # data cleaning
 # Mutate factor forms of Gun_type and Power
@@ -15,15 +15,8 @@ dat$Gun_type_f = factor(dat$Gun_type, labels = c("ZQ-5", "AR-15")) %>%
 dat$Power_f = factor(dat$Power, labels = c("Weak", "Strong")) %>% 
   C(sum) %>% 
   relevel(ref = "Weak")
-
-lm(AR_sum1 ~ Gun_type_f * Power_f, data = dat) %>% 
-  summary
-dat %>% 
-  group_by(Gun_type, Power) %>% 
-  summarize(AR_mean = mean(AR_sum1, na.rm=T))
-
-# How much cleaning is there to do?
-
+# factor form of political orientation
+dat$pol_orien_f = factor(dat$pol_orien, labels = c("Dem", "Rep", "Mod", "Lib", "Other"))
 
 # Analysis ----
 # Manipulation check
@@ -36,7 +29,8 @@ tapply(dat$p_key_value_2, list(dat$Gun_type_f, dat$Power_f), mean, na.rm = T)
 ggplot(dat, aes(x = p_key_value_1, fill = Power_f)) +
   geom_histogram(position = "dodge")
 ggplot(dat, aes(x = interaction(Power_f, Gun_type_f), y = p_key_value_1)) +
-  geom_violin()
+  geom_violin() +
+  geom_point(position = position_jitter(w = .2, h = 0.2))
 
 ggplot(dat, aes(x=p_key_value_2, fill = Power_f)) +
   geom_histogram(position = "dodge")
@@ -44,17 +38,49 @@ ggplot(dat, aes(x = interaction(Power_f, Gun_type_f), y = p_key_value_2)) +
   geom_violin() +
   geom_point(position = position_jitter(w = .15, h = 0))
 
-# Main outcomes
-dat_bayes = dat %>% 
-  select(Gun_type, Power, AR_sum1) %>% 
-  mutate(Gun_type = as.factor(Gun_type),
-         Power = as.factor(Power),
-         AR_sum1 = as.numeric(AR_sum1)) %>% 
-  filter(complete.cases(.))
-dat_bayes = data.frame(dat_bayes)
-bayes_model = anovaBF(AR_sum1 ~ Gun_type * Power, rscaleFixed = .5, data = dat_bayes)
-summary(bayes_model)
-plot(bayes_model)
-lm(AR_sum1 ~ Gun_type * Power, data = dat_bayes) %>% summary
+# How bad is data missingness across DVs?
+DVs = c("amend1", "amend2", 
+        "AR_desire", "AR_intent", "AR_price", 
+        "game_gun_desire", "policy", "magazine_cap",
+        "per_1", "per_2", "per_3", "per_4")
+for (i in DVs) {
+  print(i); print(sum(is.na(dat[,i])))
+}
 
-# Analysis, considering covariates?
+# Main outcomes
+dat1 = dat %>% 
+  select(amend2, AR_intent, game_gun_desire, policy,
+         Gun_type_f, Power_f, pol_orien_f) %>% 
+  filter(complete.cases(.)) %>% 
+  as.data.frame()
+
+mod1 = anovaBF(amend2 ~ Gun_type_f * Power_f, 
+               rscaleFixed = .5, data = dat1)
+mod2 = anovaBF(AR_intent ~ Gun_type_f * Power_f, 
+               rscaleFixed = .5, data = dat1)
+mod3 = anovaBF(game_gun_desire ~ Gun_type_f * Power_f, 
+               rscaleFixed = .5, data = dat1)
+mod4 = anovaBF(policy ~ Gun_type_f * Power_f, 
+               rscaleFixed = .5, data = dat1)
+
+
+# Adding politics as a covariate
+mod1_cov = anovaBF(amend2 ~ Gun_type_f * Power_f + pol_orien_f, 
+                   rscaleFixed = .5, data = dat1)
+mod2_cov = anovaBF(AR_intent ~ Gun_type_f * Power_f + pol_orien_f, 
+                   rscaleFixed = .5, data = dat1)
+mod3_cov = anovaBF(game_gun_desire ~ Gun_type_f * Power_f + pol_orien_f, 
+                   rscaleFixed = .5, data = dat1)
+mod4_cov = anovaBF(policy ~ Gun_type_f * Power_f + pol_orien_f, 
+                   rscaleFixed = .5, data = dat1)
+
+# AR_desire had some missing data
+mod5_cov = dat %>% 
+  select(AR_desire, Gun_type_f, Power_f, pol_orien_f) %>% 
+  filter(complete.cases(.)) %>% 
+  as.data.frame() %>% 
+  anovaBF(AR_desire ~ Gun_type_f * Power_f + pol_orien_f, 
+          rscaleFixed = .5, data = .)
+sort(mod5_cov)
+
+
